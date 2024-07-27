5e33b19fcee535b2e38b276d2da73d2ad9e53e41
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-07-27 23:02:22 +0700

    starting

---
+++ src/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3308,
+    user: 'root',
+    password: '',
+    database: 'web_asset_management'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+module.exports = connection;
