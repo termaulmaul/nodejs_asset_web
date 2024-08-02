cba249f6e5260ec6fec9d92e3d692febdcffc0ea
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-08-02 09:21:22 +0700

    fix the icon success

--- app.js
+++ app.js
@@ -1,24 +1,83 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./src/database'); // Assuming database.js is in src
+
 const app = express();
+const PORT = 3000;
 
-// Middleware to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from 'public' folder
+app.use('/assets', express.static(path.join(__dirname, 'assets'))); // Serve assets from 'assets' folder
 
-// Route for login page
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Halaman login
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
 app.post('/login', (req, res) => {
-    // Handle login logic here
-    res.redirect('/');
+    const { username, password } = req.body;
+    db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            res.redirect('/assets');
+        } else {
+            res.send('Invalid credentials.');
+        }
+    });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Halaman utama aset
+app.get('/assets', (req, res) => {
+    db.query('SELECT * FROM assets', (err, results) => {
+        if (err) throw err;
+        res.render('index', { assets: results });
+    });
+});
+
+// Menambah aset
+app.get('/add-asset', (req, res) => {
+    res.render('add-asset');
+});
+
+app.post('/add-asset', (req, res) => {
+    const { asset_class, asset_number, investment_order, asset_name, serial_number, merk, type, branch_code, branch_name, division_name, department_name, user_name, acquisition_price, acquisition_year, amortization_percentage, condition } = req.body;
+    db.query('INSERT INTO assets (asset_class, asset_number, investment_order, asset_name, serial_number, merk, type, branch_code, branch_name, division_name, department_name, user_name, acquisition_price, acquisition_year, amortization_percentage, condition) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [asset_class, asset_number, investment_order, asset_name, serial_number, merk, type, branch_code, branch_name, division_name, department_name, user_name, acquisition_price, acquisition_year, amortization_percentage, condition], (err) => {
+        if (err) throw err;
+        res.redirect('/assets');
+    });
+});
+
+// Mengedit aset
+app.get('/edit-asset/:id', (req, res) => {
+    const assetId = req.params.id;
+    db.query('SELECT * FROM assets WHERE id = ?', [assetId], (err, results) => {
+        if (err) throw err;
+        res.render('edit-asset', { asset: results[0] });
+    });
+});
+
+app.post('/edit-asset/:id', (req, res) => {
+    const assetId = req.params.id;
+    const { asset_class, asset_number, investment_order, asset_name, serial_number, merk, type, branch_code, branch_name, division_name, department_name, user_name, acquisition_price, acquisition_year, amortization_percentage, condition } = req.body;
+    db.query('UPDATE assets SET asset_class = ?, asset_number = ?, investment_order = ?, asset_name = ?, serial_number = ?, merk, type = ?, branch_code = ?, branch_name = ?, division_name = ?, department_name = ?, user_name = ?, acquisition_price = ?, acquisition_year = ?, amortization_percentage = ?, condition = ? WHERE id = ?', [asset_class, asset_number, investment_order, asset_name, serial_number, merk, type, branch_code, branch_name, division_name, department_name, user_name, acquisition_price, acquisition_year, amortization_percentage, condition, assetId], (err) => {
+        if (err) throw err;
+        res.redirect('/assets');
+    });
+});
+
+// Menghapus aset
+app.post('/delete-asset/:id', (req, res) => {
+    const assetId = req.params.id;
+    db.query('DELETE FROM assets WHERE id = ?', [assetId], (err) => {
+        if (err) throw err;
+        res.redirect('/assets');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
